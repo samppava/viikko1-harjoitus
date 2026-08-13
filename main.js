8a921fb2f693d02cff7288b827d98f93919e537b
samppavaaa <samuel.vaajala@gmail.com>
2026-08-13 14:12:10 +0300

    tehtävät

---
+++ main.js
@@ -0,0 +1,10 @@
+import { tervehdi } from "./tervehdys.js"; 
+
+
+function main() {
+console.log(tervehdi("matti"));
+console.log(tervehdi("karrar"));
+
+}
+
+main();
---
+++ tervehdys.js
@@ -0,0 +1,3 @@
+    export function tervehdi(nimi) {
+    return `Hei, ${nimi}!`;
+}
